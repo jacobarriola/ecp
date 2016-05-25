95e78d96eb46c072429e93baa35cc98be8afd1bc
Jacob Arriola <jacob@zeek.com>
2016-05-24 22:39:28 -0700

    Init static markup and styles for home page

--- wp-content/themes/ecp/front-page.php
+++ wp-content/themes/ecp/front-page.php
@@ -9,47 +9,65 @@
 
 get_header(); ?>
 
-<div class="row">
+<div class="home-hero">
+	<div class="row column">
+		<div class="home-hero-content">
+			<h2 class="hero-header">ReBoot Camp</h2>
+			<p class="hero-tagline">Define your life. Make it Happen.</p>
+		</div>
+	</div>
+</div>
 
-	<div class="medium-8 columns">
-
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
-
-			<?php if ( have_posts() ) : ?>
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'components/content', get_post_format() );
-					?>
-
-				<?php endwhile; ?>
-
-				<?php the_posts_navigation(); ?>
-
-			<?php else : ?>
-
-				<?php get_template_part( 'components/content', 'none' ); ?>
-
-			<?php endif; ?>
-
-			</main><!-- #main -->
-		</div><!-- #primary -->
-
-	</div><!-- .columns -->
+<div class="row home-what">
 
 	<div class="medium-4 columns">
+		<h3>What is Reboot Camp?</h3>
+		<div class="what-copy">
+			<p>A one-day, highly interactive workshop designed to help you re-imagine your life. It’s much-needed time and space where you can focus on the various concepts and tools we’ve devised to help you re-define what it means to live in balance, to live in alignment and turn what you’ve wanted to do or thought about doing, into what you ARE doing. It’s also meeting a  group of like-minded people, with whom you’ll  exchange ideas, give and get supportive feedback,
+				as well as the invaluable opportunity of creating an ongoing accountability community.
+			</p>
+		</div>
+		<a href="/reboot-camp" class="button expanded">Learn More</a>
+	</div>
 
-		<?php get_sidebar(); ?>
+	<div class="medium-7 columns">
+		<img src="http://fillmurray.com/800/375" alt="">
+	</div>
 
-	</div><!-- .columns -->
+</div>
 
-</div><!-- .row -->
+<div class="home-testimonial">
+	<div class="row">
+
+		<div class="large-8 columns home-testimonial-content">
+			<h3 class="testimonial-header">"Whatever you do, or dream you can, begin it. Boldness has genius and power
+			and magic behind it."</h3>
+			<cite>John Wolgang von Goethe</cite>
+		</div>
+
+	</div>
+</div>
+
+
+<div class="what-we-do">
+
+	<div class="row column">
+
+		<div class="what-we-do-content">
+
+			<div class="what-we-do-img">
+				<img src="http://fillmurray.com/600/475" alt="">
+			</div>
+
+			<div class="what-we-do-copy">
+				<h3>What Do We Do?</h3>
+				<p>We help people think more creatively about how to elevate their lives.
+In creating content for our workshops with straightforward, easy-to-use guidelines and tools, we help individuals define their own brand of success, based upon what’s important to them and what they already do well. We also provide
+a well-tested formula for an action plan, with realistic goal setting and suggested accountability tips, to ensure that each person’s success stays within their reach.</p>
+			</div>
+
+		</div>
+	</div>
+</div>
 
 <?php get_footer(); ?>
