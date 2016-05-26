f0c1d45e2064fd5e32670d1284bb8cd585c5080e
Jacob Arriola <jacob@zeek.com>
2016-05-25 23:41:08 -0700

    Add post image custom field

--- wp-content/themes/ecp/inc/custom-fields.php
+++ wp-content/themes/ecp/inc/custom-fields.php
@@ -543,5 +543,44 @@ acf_add_local_field_group(array (
 	'description' => '',
 ));
 
+acf_add_local_field_group(array (
+	'key' => 'group_57469598e8728',
+	'title' => 'Post Options',
+	'fields' => array (
+		array (
+			'key' => 'field_574695ae908d6',
+			'label' => 'Featured Image Placement',
+			'name' => 'place_featured_image_on_right',
+			'type' => 'true_false',
+			'instructions' => 'Default is the left side',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'message' => 'Place featured image on right side',
+			'default_value' => 0,
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'post',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'side',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
 
 endif;
