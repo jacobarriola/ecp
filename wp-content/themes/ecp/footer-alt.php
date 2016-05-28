af1529e5e1f72e1cf212ac4d0c1585355468322b
Jacob Arriola <jacob@zeek.com>
2016-05-27 20:24:20 -0700

    Add gravity forms

--- wp-content/themes/ecp/footer-alt.php
+++ wp-content/themes/ecp/footer-alt.php
@@ -16,19 +16,20 @@
 
 	<div class="row">
 
-		<div class="medium-centered medium-9 columns">
+		<div class="medium-centered medium-10 columns">
 
 			<div class="row">
-				<div class="medium-5 columns">
+
+				<div class="medium-4 columns">
 					<h2 class="footer-header">Contact Us</h2>
 					<p>If you have questions or comments, we'd love to hear from you.</p>
 					<ul class="footer-social">
-						<?php get_template_part( 'components/social' ); ?>	
+						<?php get_template_part( 'components/social' ); ?>
 					</ul>
 				</div>
 
 				<div class="medium-7 columns">
-
+						<?php /* gravity_form(1, false, false, false, '', true, 12); */ ?>
 				</div>
 			</div>
 
